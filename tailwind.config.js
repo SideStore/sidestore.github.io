4b3eb346c658fce3f17c49a00de272341cb8bafb
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2022-12-19 23:02:00 +0000

    add stuff

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        buttonsok: '355px',
+        stoplgtranslation: '1330px',
+        // tran,
+      },
+      colors: {
+        side: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+
+          900: '#1a202c',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
