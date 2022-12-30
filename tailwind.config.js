e98e5d8cf02bc97d0c0815de33dbc58610394887
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2022-12-30 12:43:08 +0000

    remove stuff in tailwind conf and comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,11 @@ module.exports = {
       screens: {
         buttonsok: '355px',
         stoplgtranslation: '1330px',
-        // tran,
       },
       colors: {
         side: {
           100: '#f7fafc',
           200: '#edf2f7',
-
           900: '#1a202c',
         },
       },
