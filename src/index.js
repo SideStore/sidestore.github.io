e512b29b6adfe764fd72fe551e17d589f17240a7
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-03-23 19:07:28 +0000

    refactor buttons, dynamic icons/mockevents etc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import icons from './assets/app_icons/*.webp';
+import icons from './assets/apps_data/*.webp';
+import rawApps from './assets/apps_data/applist.json';
 
 let template = (html, obj, urlkeys) => {
   Object.keys(obj).forEach(
@@ -10,80 +11,10 @@ let template = (html, obj, urlkeys) => {
 let marqueeItem = `<div class="mx-2 flex h-auto w-max max-w-[324px] items-center rounded-xl bg-[rgba(31,32,35,.3)] shadow-sm backdrop-blur-2xl px-4 py-4"><img src="{{icon}}" alt="{{name}}" class="mr-4 h-16 w-16 rounded-xl" /><div class="flex flex-col justify-center"><h1 class="text-xl font-medium font-['Poppins'] text-gray-200">{{name}}</h1><p class="whitespace-pre-line text-sm text-gray-200">{{desc}}</p></div></div>`;
 let eventItem = `<li class="flex items-center mb-1.5"><img class="mr-2 h-5 w-5 rounded-full" loading="lazy" src="{{avatar}}" alt="{{actor_name}}"/><p class="font-mono text-xs text-gray-300">{{message}}</p></li>`;
 
-let apps = [
-  { name: 'UTM', desc: 'Run virtual machines on iOS' },
-  {
-    name: 'Provenance',
-    desc: 'Multi-emulator frontend, supporting various Atari, Bandai, NEC, Nintendo, Sega, SNK and Sony console systems…',
-  },
-  { name: 'PojavLauncher', desc: 'A Minecraft: Java Edition Launcher' },
-  { name: 'Flycast', desc: 'A Dreamcast, Naomi, Naomi 2 and Atomiswave emulator.' },
-  { name: 'DolphiniOS', desc: 'A port of the popular Dolphin emulator to iOS.' },
-  {
-    name: 'PokeMMO',
-    desc: 'PokeMMO is a free to play mmorpg, come join a growing community as you level up and discover new monsters.',
-  },
-  { name: 'Delta', desc: 'Classic games in your pocket.' },
-  { name: 'Clip', desc: 'Manage your clipboard history with ease.' },
-  {
-    name: 'PPSSPP',
-    desc: 'PPSSPP can run your PSP games on your iPhone and iPad in full HD resolution.',
-  },
-  {
-    name: 'RetroArch',
-    desc: 'RetroArch is a frontend for a plethora of emulators.',
-  },
-  {
-    name: 'iNDS',
-    desc: 'Aa derivation of the previous Nintendo DS emulator apps for iOS: nds4ios and Nitrogen.',
-  },
-  {
-    name: 'Play!',
-    desc: 'Play! is a portable PlayStation2 emulator.',
-  },
-  {
-    name: 'iDOS',
-    desc: 'Play classic DOS games on your iPhone and iPad!',
-  },
-  {
-    name: 'Firebird Emu',
-    desc: 'Third-party multi-platform emulator of the ARM-based TI-Nspire calculators.',
-  },
-  {
-    name: 'ActiveGS iOS',
-    desc: 'An emulator for all things Apple.',
-  },
-  {
-    name: 'Ready',
-    desc: 'Ready is an open source emulator for multiple 8 bit home computers.',
-  },
-  {
-    name: 'MAME4iOS',
-    desc: 'Play arcade games from the past 30+ years.',
-  },
-  {
-    name: 'OldOS',
-    desc: 'OldOS is a testament to the days of yesteryear, showcasing what iOS once was ten years ago.',
-  },
-  {
-    name: 'iTorrent',
-    desc: 'iTorrent is an ordinary torrent client for iOS with Files app support.',
-  },
-  {
-    name: 'Rewound',
-    desc: 'Rewound uses your Apple Music library and displays it in the nostalgic style of an old spin-wheel iPod.',
-  },
-  {
-    name: 'ScummVM',
-    desc: 'Run certain classic graphical point-and-click adventure games and role-playing games.',
-  },
-  {
-    name: 'Mini vMac',
-    desc: 'Fully emulates the Mac Plus, the Mac II or the Mac 128K',
-  },
-];
-window.apps = apps;
 (async () => {
+  let apps = rawApps
+    .sort(() => Math.random() - 0.5);
+
   apps = apps
     .map((i) => ({ ...i, icon: icons[i.name].endsWith('.webp') ? icons[i.name] : icons[i.name] + '.webp' }))
     .map((i) => template(marqueeItem, i, ['icon']));
@@ -102,6 +33,33 @@ window.apps = apps;
   } else {
     eventLog = await (await fetch('https://api.github.com/orgs/sidestore/events?per_page=100')).json();
   }
+
+  // loop over all elements with the id "dropdown-button", get the attribute "linked-dropdown" and when the element with the id of the attribute is clicked, add the classes from the attribute "dropdown-hidden" to the element with the id of the attribute and remove the classes from the attribute "dropdown-visible" from the element with the id of the attribute and vice versa
+  document.querySelectorAll('[linked-dropdown]').forEach((el) => {
+    const linkedDropdown = document.getElementById(el.getAttribute('linked-dropdown'));
+    const dropdownHidden = linkedDropdown.getAttribute('dropdown-hidden').split(' ') || [];
+    const dropdownVisible = linkedDropdown.getAttribute('dropdown-visible').split(' ') || [];
+    const copyWidth = linkedDropdown.getAttribute('dd-copy-width') ? document.querySelector(linkedDropdown.getAttribute('dd-copy-width')) : false;
+    console.log(`Linked dropdown button to ${`#` + linkedDropdown.id || `.` + el.class}.`);
+    const hide = () => {
+      linkedDropdown.classList.remove('dd-visible');
+      dropdownHidden.forEach((i) => linkedDropdown.classList.add(i));
+      dropdownVisible.forEach((i) => linkedDropdown.classList.remove(i));
+    };
+    hide();
+    el.addEventListener('click', () => {
+      // if the linked dropdown has the class dd-visibile, then remove visible tags and add hidden tags
+      if (linkedDropdown.classList.contains('dd-visible')) hide();
+      else {
+        linkedDropdown.classList.add('dd-visible');
+        if (copyWidth) linkedDropdown.style.width = copyWidth.offsetWidth + 'px';
+        dropdownHidden.forEach((i) => linkedDropdown.classList.remove(i));
+        dropdownVisible.forEach((i) => linkedDropdown.classList.add(i));
+        //scroll to top of dropdown
+        linkedDropdown.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
 
   const typeMap = {
     PushEvent: 'commit',
@@ -175,4 +133,26 @@ window.apps = apps;
     .slice(0, 15);
 
   document.querySelectorAll('#event-log').forEach((el) => (el.innerHTML = log.join('')));
+
+  let os = 'unknown';
+
+  if (navigator.appVersion.indexOf("Win") != -1) os = "windows";
+  if (navigator.appVersion.indexOf("Mac") != -1) os = "macos";
+  if (navigator.appVersion.indexOf("X11") != -1 || navigator.appVersion.indexOf("Linux") != -1) os = "nix";
+
+
+  // for every button with id "hookPlatformDL"
+  document.querySelectorAll('#hookPlatformDL').forEach((el) => {
+    const ddList = document.querySelector(el.getAttribute('platform-dl-list'));
+    const platformDLElement = ddList.querySelector(`[dl-platform=${os}]`);
+    const platformText = el.querySelector("#platformText");
+    console.log(platformDLElement);
+    if (platformText)
+      platformText.innerText = `for ${platformDLElement.innerText}`;
+
+    if (platformDLElement.classList.contains('disabled') || platformDLElement.hasAttribute('disabled'))
+      return el.classList.add('disabled');
+    el.href = platformDLElement.href;
+
+  });
 })();
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       screens: {
         buttonsok: '355px',
         stoplgtranslation: '1330px',
+        dualbuttonok: '540px'
       },
       colors: {
         side: {
@@ -13,6 +14,15 @@ module.exports = {
           200: '#edf2f7',
           900: '#1a202c',
         },
+        "side-purple": {
+          100: "#d2c2f6",
+          200: "#b499f0",
+          300: "#a584ec",
+          400: "#9670e9",
+          "border": "#7447d4",
+          600: "#6932e0",
+          700: "#5428b3",
+        }
       },
     },
   },
