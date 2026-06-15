92dd530e7264d68387e0c92a2d0f7b44b72ce68a
August <cooptheloop8@gmail.com>
2026-06-15 14:42:14 -0500

    round hour ago message to prevent "1 hours ago"
    it triggered me lmao idk why.
    also basically if it was past 1 hour mark but before 1.5 hours (making it 2 hours) it would display 1 hours ago
    
    Signed-off-by: August <cooptheloop8@gmail.com>

--- src/index.js
+++ src/index.js
@@ -192,7 +192,7 @@ const repoItem = `<a style="--custom-index:{{index}}" href="{{url}}" target="_bl
       if (dateSpan == 0) set('just now');
       else if (dateSpan == 1) set('a minute ago');
       else if (dateSpan <= 60) set(`${dateSpan} minutes ago`);
-      else if (dateSpan / 60 == 1) set('an hour ago');
+      else if (Math.round(dateSpan / 60) == 1) set('an hour ago');
       else if (dateSpan / 60 <= 24) set(`${Math.round(dateSpan / 60)} hours ago`);
       else if (dateSpan / 60 <= 48) set('yesterday');
       else if (dateSpan / 60 <= 24 * 365) set(`${Math.round(dateSpan / 60 / 24)} days ago`);
